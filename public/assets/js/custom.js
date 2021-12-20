e2d95e84a1348572563521a9b43fd421a7873140
dusan.veselinovic.dev gmail.com <dusan.veselinovic.dev@gmail.com>
2021-12-20 22:16:40 +0100

    Add content of site Add firebase deployment

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -116,7 +116,7 @@ $(function() {
   });
 
   $(document).ready(function () {
-      $(document).on("scroll", onScroll);
+      // $(document).on("scroll", onScroll);
       
       //smoothscroll
       $('.scroll-to-section a[href^="#"]').on('click', function (e) {
@@ -135,25 +135,25 @@ $(function() {
               scrollTop: (target.offset().top) + 1
           }, 500, 'swing', function () {
               window.location.hash = target;
-              $(document).on("scroll", onScroll);
+              // $(document).on("scroll", onScroll);
           });
       });
   });
 
-  function onScroll(event){
-      var scrollPos = $(document).scrollTop();
-      $('.nav a').each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-              $('.nav ul li a').removeClass("active");
-              currLink.addClass("active");
-          }
-          else{
-              currLink.removeClass("active");
-          }
-      });
-  }
+  // function onScroll(event){
+  //     var scrollPos = $(document).scrollTop();
+  //     $('.nav a').each(function () {
+  //         var currLink = $(this);
+  //         var refElement = $(currLink.attr("href"));
+  //         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+  //             $('.nav ul li a').removeClass("active");
+  //             currLink.addClass("active");
+  //         }
+  //         else{
+  //             currLink.removeClass("active");
+  //         }
+  //     });
+  // }
 
 
   // Acc
